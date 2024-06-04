7d18e810131d8447b306a3a99a4f3bde71262c27
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-04 17:27:22 +0300

    Added the autocomplete field

--- Client-01/src/containers/tutor-home-page/general-info-step/GeneralInfoStep.tsx
+++ Client-01/src/containers/tutor-home-page/general-info-step/GeneralInfoStep.tsx
@@ -2,12 +2,29 @@ import Box from '@mui/material/Box'
 import { styles } from '~/containers/tutor-home-page/general-info-step/GeneralInfoStep.styles'
 import { IGeneralInfoStep } from '~/containers/tutor-home-page/general-info-step/interfaces/GeneralInfoStep'
 import React from 'react'
-
+import boyUnderLamp from '~/assets/img/tutor-home-page/become-tutor/general-info.svg'
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
 const GeneralInfoStep: React.FC<IGeneralInfoStep> = ({ btnsBox }) => {
   return (
     <Box sx={styles.container}>
-      GeneralInfo step
-      {btnsBox}
+      <Box>
+        <img alt='The boy under the lamp' src={boyUnderLamp} />
+      </Box>
+      <Box>
+        <form>
+          <AsyncAutocomplete
+            axiosProps={undefined}
+            fetchCondition={undefined}
+            fetchOnFocus={undefined}
+            labelField={undefined}
+            service={undefined}
+            textFieldProps={undefined}
+            value={undefined}
+            valueField={undefined}
+          />
+        </form>
+        {btnsBox}
+      </Box>
     </Box>
   )
 }
